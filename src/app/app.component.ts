dcee8e9365deb1dac41d41d35dc3456e6f47b8a2
Texterly <88582978+Texterly@users.noreply.github.com>
2024-02-22 14:22:40 +0300

    added calculate logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,37 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'first-app';
   mode: boolean = true;
-  currValue: string = '0';
+  currValue: string = '';
 
   onButtonClicked(value: string) {
     this.writeToInput(value);
   }
 
   writeToInput(value: string) {
-    this.currValue = this.currValue + value;
+    switch (value) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+      case '-':
+      case '+':
+      case '*':
+      case '/':
+        this.currValue = this.currValue + value;
+        break;
+      case '=':
+        this.currValue = eval(this.currValue);
+        break;
+      case 'c':
+        this.currValue = '';
+        break;
+    }
   }
 }
 
